e46f12ac26e5e2060330630055676be06ce741fa
Paul Foos <paul.foos@umb.edu>
2019-03-07 15:46:29 -0500

    second commit of NERCVE8

---
+++ web/themes/custom/nercve_boot4/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.nercve_boot4 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
